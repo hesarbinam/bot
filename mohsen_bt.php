b12f03f82821c51fa9f9bf3b0b6eef171cf45be5
Mohsen <72359061+hesarbinam@users.noreply.github.com>
2020-11-30 22:45:53 +0330

    Add files via upload

---
+++ mohsen_bt.php
@@ -0,0 +1,38 @@
+<?php
+
+	// https://api.telegram.org/bot1244263877:AAE7yiPwxxnuremNPSKsU4hVsMY8c1l5urk/setwebhook?url=http://mohsenkh.ss0.ir/bot/mohsen_bt.php
+	$update = file_get_contents("php://input");
+	
+	$update_array = json_decode($update , true);
+	
+	if (isset($update_array["message"]))
+	{
+		$text = $update_array["message"]["text"];
+		
+		$chat_id = $update_array["message"]["chat"]["id"];
+		
+	}
+	
+	$reply = "پیام شما : ".$GLOBALS['text'];
+	$url = "https://api.telegram.org/bot" ."1244263877:AAE7yiPwxxnuremNPSKsU4hVsMY8c1l5urk" . "/sendMessage";
+	
+	$post_params = ['chat_id' => $GLOBALS['chat_id'] , 'text' => $reply];
+	send_replay($url , $post_params);
+	
+	
+	function send_replay($url , $post_params)
+	{
+		$cu = curl_init();
+		
+		curl_setopt($cu , CURLOPT_URL , $url);
+		curl_setopt($cu , CURLOPT_POSTFIELDS , $post_params);
+		curl_setopt($cu , CURLOPT_RETURNTRANSFER, true);
+		
+		$result = curl_exec($cu);
+		
+		curl_close($cu);
+		
+		return $result;
+	}
+
+?>
